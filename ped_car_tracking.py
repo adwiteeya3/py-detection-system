f9330a9c495ad2858f12fa83a798dc1423a949b7
Adwiteeya <adwiteeya3@gmail.com>
2020-08-03 22:43:02 +0530

    image identifier done

--- ped_car_tracking.py
+++ ped_car_tracking.py
@@ -19,14 +19,14 @@ car_tracker= cv2.CascadeClassifier(classifier_file)
 cars= car_tracker.detectMultiScale(black_n_white)
 
 #drawing rectangles and squares around the cars
-for (x,y,w,h):
+for (x,y,w,h) in cars:
     cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), 2)
 
 #print(cars)
 
 
 #display the image with the faces spotted in black and white
-cv2.imshow('ped_car_tracking', black_n_white)
+cv2.imshow('ped_car_tracking', img)
 
 #display for some time
 cv2.waitKey()
