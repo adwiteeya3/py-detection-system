2f71eed0527a236d185e251fa2140e81537e9091
Adwiteeya <adwiteeya3@gmail.com>
2020-08-03 22:02:03 +0530

    updates

--- ped_car_tracking.py
+++ ped_car_tracking.py
@@ -6,4 +6,18 @@ img_file= "images\car2.jpg"
 #pre-trained car classifier
 classifier_file= 'car_classifier.xml'
 
+#create opencv image
+img= cv2.imread(img_file)
+
+#create car classifier
+car_tracker= cv2.CascadeClassifier(classifier_file)
+
+
+
+#display the image with the faces spotted
+cv2.imshow('ped_car_tracking', img)
+
+#display for some time
+cv2.waitKey()
+
 print('so far so good!')
