d877c18b91c96efd60b9cd0a2b68e20f0d3af37f
Adwiteeya <adwiteeya3@gmail.com>
2020-08-04 16:49:42 +0530

    pedestrian classified

--- ped_car_tracking.py
+++ ped_car_tracking.py
@@ -2,13 +2,16 @@ import cv2
 
 #Image file
 img_file= "images\cars8.jpg"
-video= cv2.VideoCapture('images\car_video.mp4')
+#video= cv2.VideoCapture('images\car_video.mp4')
+video= cv2.VideoCapture('images\ped_video.mp4')
 
-#pre-trained car classifier
-classifier_file= 'car_classifier.xml'
+#pre-trained car and pedestrian classifier
+car_tracker_file= 'car_classifier.xml'
+pedestrian_tracker_file= 'ped_classifier.xml'
 
-#create car classifier
-car_tracker= cv2.CascadeClassifier(classifier_file)
+#create car and pedestrian classifier
+car_tracker= cv2.CascadeClassifier(car_tracker_file)
+pedestrian_tracker= cv2.CascadeClassifier(pedestrian_tracker_file)
 
 #run forever until car stops
 while True:
@@ -21,18 +24,30 @@ while True:
     else:
         break
 
-    #detect cars
+    #detect cars and pedestrians
     cars= car_tracker.detectMultiScale( grayscaled_frame)
+    pedestrians= pedestrian_tracker.detectMultiScale( grayscaled_frame)
 
     #drawing rectangles and squares around the cars
     for (x,y,w,h) in cars:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
-
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+    
+    #drawing rectangles and squares around the pedestrians
+    for (x,y,w,h) in pedestrians:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)    
+    
     #display the image with the faces spotted in black and white
     cv2.imshow('ped_car_tracking', frame)
 
     #display for some time
-    cv2.waitKey(1)
+    key= cv2.waitKey(1)
+
+    #stop if Q or q key is pressed
+    if key==81 or key==113:
+        break
+
+#Release the videocapture object
+video.release()
 
 
 
@@ -64,4 +79,4 @@ cv2.waitKey()
 
 '''
 
-print('so far so good!')
+#print('so far so good!')
